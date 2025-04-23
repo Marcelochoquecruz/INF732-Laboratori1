2e011b2815cb1fc0dbe994038ed3850942c4dec7
Marcelo <chelisimo27@gmail.com>
2025-04-23 09:48:04 -0600

    corregido

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,9 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 describe('AppController', () => {
   let appController: AppController;
+
+  const mockResponse = () => {
+    const res: Partial<Response> = {};
+    res.send = jest.fn().mockReturnThis();
+    return res as Response;
+  };
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -15,8 +22,12 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return an HTML response with "Hola Mundo"', () => {
+      const res = mockResponse();
+      appController.getHello(res);
+
+      expect(res.send).toHaveBeenCalledWith(expect.stringContaining('Hola Mundo'));
+      expect(res.send).toHaveBeenCalledWith(expect.stringContaining('<h1'));
     });
   });
 });
